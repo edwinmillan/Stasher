8405f94b9ba12a5a4bf680062625030a7a92bee9
Edwin Millan <bassgs3000@gmail.com>
2017-09-25 00:43:38 -0500

    Refined stashing ability

--- stasher.py
+++ stasher.py
@@ -1,7 +1,7 @@
 import os
 import hashlib
 import argparse
-from tinydb import TinyDB, where
+from tinydb import TinyDB, Query
 from pprint import pprint
 from shutil import move
 
@@ -10,13 +10,12 @@ def get_args():
     parser = argparse.ArgumentParser(description='Duplicate checker')
     parser.add_argument('-s', '--source', default=os.getcwd(), help='Path to source dir, defaults to current directory')
     parser.add_argument('-t', '--target', help='Path to target dir, defaults to source directory')
-    parser.add_argument('-b', '--build-inventory', action='store_true', help='Builds inventory file')
+    parser.add_argument('-r', '--rebuild-inventory', action='store_true', help='Rebuilds inventory file')
 
     args = parser.parse_args()
     # Set target to the value of source if it's not provided.
     args.target = args.target if args.target else args.source
 
-    pprint(args)
     return args
 
 
@@ -29,10 +28,10 @@ def chunk_reader(file, chunk_size=1024):
         yield datachunk
 
 
-def gather_inventory(source_path):
+def gather_inventory(path):
     database = []
     duplicates = []
-    for dirpath, dirnames, filenames in os.walk(source_path):
+    for dirpath, dirnames, filenames in os.walk(path):
         for filename in filenames:
             data = {'filename': filename}
             # Create an empty hashlib hash for filling
@@ -56,7 +55,6 @@ def gather_inventory(source_path):
             if duplicate_found:
                 # Report and Delete the data
                 print(f'Duplicate found: [{data["hash"][:7]}] {data["filename"]}')
-                print(f'\tMarking for Deletion: {data["full_path"]}')
                 duplicates.append(data)
             else:
                 # Store the collected data in the database
@@ -66,25 +64,61 @@ def gather_inventory(source_path):
 
 def delete_duplicates(duplicates_list):
     for duplicate in duplicates_list:
-        print(f'Deleting {duplicate["filename"]}')
+        print(f'Deleting [{duplicate["hash"][:7]}] {duplicate["filename"]}')
         os.remove(duplicate['full_path'])
+
+
+def build_inventory(inventory_filepath, data):
+    with TinyDB(inventory_filepath) as db:
+        for datum in data:
+            db.insert(datum)
+
+
+def stash(database, target_path, inventory_filepath):
+    for entry in database:
+        if entry['full_path'] == inventory_filepath:
+            print("Removing Inventory from Database")
+            database.pop(entry)
+    with TinyDB(inventory_filepath) as db:
+        duplicates = []
+        for file_entry in database:
+            entry = Query()
+            match = db.search(entry['hash'] == file_entry['hash'])
+            if match:
+                print(f'Duplicate found: [{file_entry["hash"][:7]}] {file_entry["filename"]}')
+                duplicates.append(file_entry)
+            else:
+                # Move the file
+                print('Moving:', file_entry['full_path'], '\n\tTo:', target_path)
+                move(file_entry['full_path'], target_path)
+                db.insert(file_entry)
+        delete_duplicates(duplicates)
 
 
 def main():
     args = get_args()
-    source_path = args.source.replace('\\', '\\\\')
     target_path = args.target.replace('\\', '\\\\')
+    source_path = args.source.replace('\\', '\\\\')
+    inventory_filename = 'inventory.json'
+    inventory_filepath = os.path.join(target_path, inventory_filename)
 
-    database, duplicates = gather_inventory(source_path)
+    if os.path.isfile(inventory_filepath):
+        print(inventory_filepath, 'exists! Using it.')
+    else:
+        print(inventory_filepath, 'doesn\'t exist! Building', inventory_filename)
+        target_database, target_duplicates = gather_inventory(target_path)
+        if target_duplicates:
+            delete_duplicates(target_duplicates)
+        build_inventory(inventory_filepath, target_database)
 
-    if duplicates:
-        delete_duplicates(duplicates)
+    source_database, source_duplicates = gather_inventory(source_path)
+    if source_duplicates:
+        delete_duplicates(source_duplicates)
 
-    # if args.build_inventory:
-    #     pass
+    stash(source_database, target_path, inventory_filepath)
 
-    # pprint(database)
-    # pprint(duplicates)
+    print("Done")
+    # input("Done")
 
 
 if __name__ == '__main__':
